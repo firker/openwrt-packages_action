ce725288cb3d49dc9c1464ab336bc05e6ea003d3
firkerword <61251923+firkerword@users.noreply.github.com>
2023-02-18 14:19:26 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,12 +4,14 @@ rm -rf ./cpulimit
 rm -rf ./luci-app-cpulimit        
 rm -rf ./luci-app-onliner
 rm -rf ./smartdns
+rm -rf ./luci-theme-design
           
 svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
 svn co https://github.com/haiibo/openwrt-packages/trunk/cpulimit
 svn co https://github.com/haiibo/openwrt-packages/trunk/luci-app-cpulimit
 svn co https://github.com/haiibo/openwrt-packages/trunk/luci-app-onliner
 git clone https://github.com/pymumu/openwrt-smartdns.git smartdns
+git clone https://github.com/gngpp/luci-theme-design.git theme/luci-theme-design
 sed -i 's/默认用户名密码都是root/默认用户名密码都是AdGuardHome/g' ./adguardhome/patches/default_username.patch 
 
 rm -rf .svn
