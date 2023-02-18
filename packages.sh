4ca47164e08e8bd124033c67966f8a09e1c22c15
firkerword <61251923+firkerword@users.noreply.github.com>
2023-02-18 13:35:42 +0800

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+rm -rf ./adguardhome     
+rm -rf ./cpulimit        
+rm -rf ./luci-app-cpulimit        
+rm -rf ./luci-app-onliner
+rm -rf ./smartdns
+          
+svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
+svn co https://github.com/haiibo/openwrt-packages/trunk/cpulimit
+svn co https://github.com/haiibo/openwrt-packages/trunk/luci-app-cpulimit
+svn co https://github.com/haiibo/openwrt-packages/trunk/luci-app-onliner
+git clone https://github.com/pymumu/openwrt-smartdns.git smartdns
+sed -i 's/默认用户名密码都是root/默认用户名密码都是AdGuardHome/g' ./adguardhome/patches/default_username.patch 
+
+rm -rf .svn
+rm -rf ./*/.git
+rm -rf ./*/.svn 
+rm -f .gitattributes .gitignore
+rm -rf .github
+rm LICENSE
+mv LICENSE.packages LICENSE
+exit 0
