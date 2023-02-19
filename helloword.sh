1fb10af5f22ff2e0b6682fe739d3b48da1faea55
firkerword <61251923+firkerword@users.noreply.github.com>
2023-02-19 15:23:28 +0800

    Update helloword.sh

--- helloword.sh
+++ helloword.sh
@@ -8,7 +8,6 @@ git clone https://github.com/xiaorouji/openwrt-passwall2.git luci-app-passwall2
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
 
 rm -rf .svn
-rm -rf .git
 rm -rf ./*/.git
 rm -rf ./*/.svn 
 rm -f .gitattributes .gitignore
