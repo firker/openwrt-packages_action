1f556da6004131537510e0ae10567075f9ef5060
firkerword <61251923+firkerword@users.noreply.github.com>
2023-02-19 15:26:13 +0800

    Update helloword.sh

--- helloword.sh
+++ helloword.sh
@@ -5,7 +5,7 @@ git clone https://github.com/fw876/helloworld.git
 git clone https://github.com/immortalwrt/homeproxy.git luci-app-homeproxy
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/xiaorouji/openwrt-passwall2.git luci-app-passwall2
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git
 
 rm -rf .svn
 rm -rf ./*/.git
