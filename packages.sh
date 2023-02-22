6f0f2eee12eec919180455ed22f13ebe6c77a1cd
firker <56651759+firker@users.noreply.github.com>
2023-02-22 10:47:10 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf adguardhome cpulimit luci-app-cpulimit luci-app-onliner smartdns
+rm -rf adguardhome cpulimit luci-app-cpulimit luci-app-onliner smartdns luci-app-homeassistant
 rm -rf ./theme/luci-theme-design
 
 svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
@@ -8,6 +8,7 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-autotimeset
 svn co https://github.com/haiibo/openwrt-packages/trunk/cpulimit
 svn co https://github.com/haiibo/openwrt-packages/trunk/luci-app-cpulimit
 svn co https://github.com/haiibo/openwrt-packages/trunk/luci-app-onliner
+svn co https://github.com/linkease/istore-packages/trunk/luci-app-homeassistant
 git clone https://github.com/pymumu/openwrt-smartdns.git smartdns
 git clone https://github.com/gngpp/luci-theme-design.git theme/luci-theme-design
 sed -i 's/默认用户名密码都是root/默认用户名密码都是admin/g' adguardhome/patches/default_username.patch 
