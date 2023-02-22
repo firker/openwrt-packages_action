0f95176b8d8aade5ea87dd172aa96acd10635f0d
firker <56651759+firker@users.noreply.github.com>
2023-02-22 13:04:08 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -13,9 +13,10 @@ git clone https://github.com/pymumu/openwrt-smartdns.git smartdns
 git clone https://github.com/gngpp/luci-theme-design.git theme/luci-theme-design
 git clone https://github.com/uparrows/luci-theme-atmaterial.git theme/luci-theme-atmaterial
 git clone https://github.com/kenzok8/luci-theme-ifit.git theme/luci-theme-ifit
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git theme/luci-theme-opentopd
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git /theme/luci-theme-opentopd
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git theme/luci-theme-infinityfreedom
-git clone https://github.com/thinktip/luci-theme-neobird.git theme/luci-theme-neobird
+git clone https://github.com/thinktip/luci-theme-neobird.git /theme/luci-theme-neobird
+git clone https://github.com/sirpdboy/luci-theme-kucat.git /theme/luci-theme-kucat
 
 sed -i 's/默认用户名密码都是root/默认用户名密码都是admin/g' adguardhome/patches/default_username.patch 
 sed -i 's/\<control\>/system/g' luci-app-autotimeset/luasrc/controller/autotimeset.lua
