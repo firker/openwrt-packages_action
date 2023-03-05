9a351d7035d8aeb2901d22e5cbcc1aa7ff666410
firkerword <61251923+firkerword@users.noreply.github.com>
2023-03-05 18:30:16 +0800

    Update helloword.sh

--- helloword.sh
+++ helloword.sh
@@ -3,8 +3,6 @@
 git clone https://github.com/immortalwrt/homeproxy.git luci-app-homeproxy
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
-source /luci-app-passwall/Makefile
-sed -i "s/$(PKG_VERSION)/$(PKG_VERSION).$(PKG_RELEASE)/g" ./luci-app-passwall/Makefile
 git clone https://github.com/firkerword/luci-app-smartdns.git
 git clone https://github.com/firkerword/smartdns.git
 
