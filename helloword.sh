ea3be54bf783bf6144307b853a56d64b92f54f66
firkerword <61251923+firkerword@users.noreply.github.com>
2023-03-10 15:49:18 +0800

    Update helloword.sh

--- helloword.sh
+++ helloword.sh
@@ -8,7 +8,7 @@ git clone -b 18.06 https://github.com/zxlhhyccc/luci-app-v2raya.git
 git clone https://github.com/firkerword/luci-app-smartdns.git
 git clone https://github.com/firkerword/smartdns.git
 # sed -i "s/PKG_VERSION:=/PKG_VERSION:=`date +%m-%d`./g" ./luci-app-passwall/Makefile
-sed -i "/PKG_VERSION/s/$/ `date +%m-%d`/" ./luci-app-passwall/Makefile
+sed -i "/PKG_VERSION/s/$/ `date +%m%d%H`/" ./luci-app-passwall/Makefile
 rm -rf .svn
 rm -rf ./*/.git
 rm -rf ./*/.svn 
