d3ac2af282baf32e485dd77f3e29624b23e841f4
firkerword <61251923+firkerword@users.noreply.github.com>
2023-03-21 11:00:09 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/firkerword/luci-app-lucky.git luci-app-lucky
+git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/firkerword/smartdns.git package/smartdns
