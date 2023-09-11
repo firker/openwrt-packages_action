86fadb5e96017da62bb983be96574d6130866367
firkerword <61251923+firkerword@users.noreply.github.com>
2023-09-11 17:44:11 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/firkerword/luci-app-lucky.git luci-app-lucky
 git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/firkerword/smartdns.git package/smartdns
-git clone https://github.com/xiaorouji/openwrt-passwall.git
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/fw876/helloworld.git && mv -n helloworld/luci-app-ssr-plus ./
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
 # git clone -b luci_unified_style https://github.com/nftbty/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
