1949f992e349b9558a405e6a6ed4d220c4d379ec
firkerword <61251923+firkerword@users.noreply.github.com>
2023-09-11 22:45:10 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -3,8 +3,8 @@
 git clone https://github.com/firkerword/luci-app-lucky.git luci-app-lucky
 git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/firkerword/smartdns.git package/smartdns
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/fw876/helloworld.git && mv -n helloworld/luci-app-ssr-plus ./
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
+git clone https://github.com/xiaorouji/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
 # git clone -b luci_unified_style https://github.com/nftbty/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
 # sed -i "/PKG_VERSION/s/$/-1/" ./luci-app-passwall/Makefile
