057a3db81702550ca66a69f2d9c13b6499e4bdaf
firker <56651759+firker@users.noreply.github.com>
2024-09-16 05:24:16 +0800

    Update packages2305.sh

--- packages2305.sh
+++ packages2305.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf package luci-app-cpulimit luci-app-onliner smartdns luci-app-smartdns
-rm -rf ./theme/*
-rm -rf ./*/luci-theme-design ./*/luci-theme-atmaterial ./*/luci-theme-ifit ./*/luci-theme-opentopd ./*/luci-theme-neobird ./*/luci-theme-infinityfreedom ./*/luci-theme-kucat ./*/luci-app-design-config 
+# rm -rf ./theme/*
+rm -rf ./*/luci-theme-design ./*/luci-theme-atmaterial ./*/luci-theme-ifit ./*/luci-theme-opentopd ./*/luci-theme-neobird ./*/luci-theme-infinityfreedom ./*/luci-theme-kucat ./*/luci-app-design-config ./*/luci-theme-kucat
 
 function merge_package(){
     repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
@@ -26,14 +26,14 @@ merge_package https://github.com/sirpdboy/sirpdboy-package sirpdboy-package/luci
 merge_package https://github.com/linkease/istore-packages istore-packages/luci-app-homeassistant
 # git clone https://github.com/sirpdboy/luci-app-autotimeset.git luci-app-autotimeset
 git clone https://github.com/pymumu/openwrt-smartdns.git smartdns
-git clone https://github.com/gngpp/luci-theme-design.git theme/luci-theme-design
-git clone https://github.com/gngpp/luci-app-design-config.git theme/luci-theme-design-config
+git clone https://github.com/MilesPoupart/luci-theme-design.git theme/luci-theme-design
+git clone https://github.com/MilesPoupart/luci-app-design-config.git theme/luci-theme-design-config
 git clone https://github.com/uparrows/luci-theme-atmaterial.git theme/luci-theme-atmaterial
 git clone https://github.com/kenzok8/luci-theme-ifit.git theme/luci-theme-ifit
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git theme/luci-theme-opentopd
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git theme/luci-theme-infinityfreedom
 git clone https://github.com/thinktip/luci-theme-neobird.git theme/luci-theme-neobird
-git clone https://github.com/sirpdboy/luci-theme-kucat.git theme/luci-theme-kucat
+git clone https://github.com/sirpdboy/luci-theme-kucat -b js theme/luci-theme-kucat
 chmod 755 luci-app-adguardhome/root/etc/init.d/AdGuardHome
 
 sed -i 's/默认用户名密码都是root/默认用户名密码都是admin/g' package/adguardhome/patches/default_username.patch 
