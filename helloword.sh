2e8765ca74b480eea1bca853bea03074d2dedfaf
firker <56651759+firker@users.noreply.github.com>
2024-11-18 22:11:25 +0800

    Update helloword.sh

--- helloword.sh
+++ helloword.sh
@@ -5,6 +5,7 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/fw876/helloworld.git && mv -n helloworld/luci-app-ssr-plus ./
 git clone https://github.com/immortalwrt/homeproxy.git luci-app-homeproxy
 git clone -b 18.06 https://github.com/zxlhhyccc/luci-app-v2raya.git
+git clone https://github.com/QiuSimons/luci-app-daed daed
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git
 git clone https://github.com/xiaorouji/openwrt-passwall2.git luci-app-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
